9836428283a42ae9b3a25523fa46484ff1060e7e
milapsingh <milap_singh@hotmail.com>
2023-08-18 03:13:35 +0530

    utils liberary with host verification with txt record

---
+++ src/app/utils/host_verfication.py
@@ -0,0 +1,27 @@
+import dns.resolver
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+class AuthorizeHost:
+    def verfiyHost(domain):
+        try:
+            result = dns.resolver.resolve(domain, 'TXT')
+            answer_raw = result.chaining_result.answer.to_text()
+            logging.info('Domain is exist with TXT record {}'.format(answer_raw))
+            return domain, "Domain verified"
+        except Exception as e:
+            logging.error("Domain nont verified with {}".format(e))
+
+    def verfiySingature(domain, prjDomin):
+        try:
+            result = dns.resolver.resolve(domain, 'TXT')
+            answer_raw = result.chaining_result.answer.to_text()
+            domainValue = answer_raw.split('=')
+            if prjDomin == domainValue[1][:len(domainValue[1])-1] and prjDomin != "":
+                logging.info("Singature verfied")
+                return 200, "Singature verified"
+            else:
+                logging.info("Singature not verified")
+                return 500, "Singature not verfied"
+        except Exception as e:
+            logging.error("Domain nont verified with {}".format(e))
