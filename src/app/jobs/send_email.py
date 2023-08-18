577149a5994c7e6168cd81f263466313ac2aba35
Madhu GB <me@madhugb.com>
2023-08-18 07:49:08 +0530

    fix: send email

--- src/app/jobs/send_email.py
+++ src/app/jobs/send_email.py
@@ -1,14 +1,14 @@
 from celery import shared_task
-from config import *
 import requests
+from config import *
 
 @shared_task(ignore_result=True)
 def send_user_confirmation(data):
     response = requests.post("https://api.mailgun.net/v2/%s/messages" % Config.MAILGUN_DOMAIN,
         auth=("api", Config.MAILGUN_KEY),
         data={
-            "from": "%s@%s" % (Config.MAILGUN_DEFAULT_USER, Config.MAILGUN_DOMAIN),
-            "to": data["email"],
+            "from": data["from"],
+            "to": data["to"],
             "subject": data["subject"],
             "text": data["text"],
             "html": data["html"]
